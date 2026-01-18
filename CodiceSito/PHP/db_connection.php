8f8cace0f7e722c2411035ccd994928956ed2872
Obliteratore <aaron.gingillino@studenti.unipd.it>
2026-01-18 16:20:38 +0100

    corretto piccoli errori, sistemato immagini, sistemato link

--- CodiceSito/PHP/db_connection.php
+++ CodiceSito/PHP/db_connection.php
@@ -2,20 +2,19 @@
 namespace FM;
 
 class FMAccess {
-	/*
+
 	private const HOST_DB = "localhost";
 	private const DATABASE_NAME = "agingill";
 	private const USERNAME = "agingill";
 	private const PASSWORD = "Pech3pheeXie4xen";
+
 	/*
-	/*
-	*/
-	
 	private const HOST_DB = "localhost";
 	private const DATABASE_NAME = "fbalestr";
 	private const USERNAME = "fbalestr";
 	private const PASSWORD = "Iemao4Chawiechoo";
-	
+	*/
+
 	/*
 	private const HOST_DB = "localhost";
 	private const DATABASE_NAME = "bsabic";
